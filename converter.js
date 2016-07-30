6b7fd2395ebe33d5c2455bf0a5ac2845996db5dd
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-07-30 13:44:35 -0500

    converter exercise

---
+++ converter.js
@@ -0,0 +1,22 @@
+function toCelsius (yourTemp) {
+	return (yourTemp - 32);
+}
+
+function toFahrenheit (yourTemp) {
+	return (yourTemp + 32);
+}
+
+// Get a reference to the button element in the DOM
+var button = document.getElementById("converter");
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+function determineConverter (clickEvent) {
+  console.log("event", clickEvent);
+}
+
+// Assign a function to be executed when the button is clicked
+button.addEventListener("click", determineConverter);
+
+
+console.log(toCelsius(32));
