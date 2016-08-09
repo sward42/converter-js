73bb67c6a4fa744c48c2a503f5d19dec306e7670
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-08-08 21:18:01 -0500

    beginning functionality

--- converter.js
+++ converter.js
@@ -1,12 +1,17 @@
 var userTemp = document.getElementById("userTemp");
 
+var fahrBut = document.getElementById("farBut");
+var celBut = document.getElementById("celBut");
 
-function toCelsius (yourTemp) {
-	return (yourTemp - 32);
+var finalTemp = document.getElementById("output");
+
+
+function toCelsius (userTemp) {
+	return ((userTemp - 32) * .56);
 }
 
-function toFahrenheit (yourTemp) {
-	return (yourTemp + 32);
+function toFahrenheit (userTemp) {
+	return ((userTemp * 1.8) + 32);
 }
 
 // Get a reference to the button element in the DOM
@@ -16,8 +21,20 @@ var button = document.getElementById("converter");
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
 	
-  console.log("event", clickEvent);
+  console.log("the button clicked");
+
+  if (fahrBut.checked === true && userTemp.value !== "") {
+  	var celTemp = toCelsius(userTemp.value);
+  	finalTemp.value = +celTemp.toFixed(1);	
+  } else if (celBut.checked === true && userTemp.value !== "") {
+  	var fahrTemp = toFahrenheit(userTemp.value);
+  	finalTemp.value = +fahrTemp.toFixed(1);
+  } else {
+  	alert("Please enter a temperature and select a scale.");
+  }
 }
+
+
 
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
