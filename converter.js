d74e8f054c19740df2956ace6f206e1ee16fcc8d
Sarah Ward <sarahward@Sarahs-MacBook-Air.local>
2016-08-09 20:52:33 -0500

    functional, added css

--- converter.js
+++ converter.js
@@ -17,6 +17,7 @@ function toFahrenheit (userTemp) {
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
 
+var clearButton = document.getElementById("clear");
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
@@ -25,18 +26,45 @@ function determineConverter (clickEvent) {
 
   if (fahrBut.checked === true && userTemp.value !== "") {
   	var celTemp = toCelsius(userTemp.value);
-  	finalTemp.value = +celTemp.toFixed(1);	
+  	finalTemp.value = +celTemp.toFixed(1);
+
   } else if (celBut.checked === true && userTemp.value !== "") {
   	var fahrTemp = toFahrenheit(userTemp.value);
   	finalTemp.value = +fahrTemp.toFixed(1);
+    
   } else {
   	alert("Please enter a temperature and select a scale.");
   }
+
+
+  if (celTemp > 32) {
+    finalTemp.style.backgroundColor = "red";
+  } else if (celTemp < 0) {
+    finalTemp.style.backgroundColor = "lightBlue";
+  } else if (celTemp < 32 && celTemp > 0) {
+    finalTemp.style.backgroundColor = "#76E970";
+  }
+
+  if (fahrTemp > 90) {
+    finalTemp.style.backgroundColor = "red"; 
+  } else if (fahrTemp < 32) {
+    finalTemp.style.backgroundColor = "lightBlue";
+  } else if (fahrTemp < 90 && fahrTemp > 32) {
+    finalTemp.style.backgroundColor = "#76E970";
+  }
+
 }
 
 
 
+function clearForm() {
+  userTemp.value = "";
+  finalTemp.value = "";
+  finalTemp.style.backgroundColor = "";
+}
+
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
 
+clearButton.addEventListener("click", clearForm);
 
